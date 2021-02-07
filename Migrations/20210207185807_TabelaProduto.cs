a47a948df26d9f720b8a96870bee626052114838
ederson-hub <prof.ederson@gmail.com>
2021-02-07 19:58:22 -0300

    First upload

---
+++ Migrations/20210207185807_TabelaProduto.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class TabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    quantidade = table.Column<int>(type: "int", nullable: false),
+                    Produtoid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Produtos_Produtoid",
+                        column: x => x.Produtoid,
+                        principalTable: "Produtos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_Produtoid",
+                table: "Produtos",
+                column: "Produtoid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ Migrations/20210207191808_produtos.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoMVC.Migrations
+{
+    public partial class produtos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Produtos_Produtos_Produtoid",
+                table: "Produtos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Produtos_Produtoid",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Produtoid",
+                table: "Produtos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Produtoid",
+                table: "Categorias",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categorias_Produtoid",
+                table: "Categorias",
+                column: "Produtoid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categorias_Produtos_Produtoid",
+                table: "Categorias",
+                column: "Produtoid",
+                principalTable: "Produtos",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categorias_Produtos_Produtoid",
+                table: "Categorias");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categorias_Produtoid",
+                table: "Categorias");
+
+            migrationBuilder.DropColumn(
+                name: "Produtoid",
+                table: "Categorias");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Produtoid",
+                table: "Produtos",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_Produtoid",
+                table: "Produtos",
+                column: "Produtoid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Produtos_Produtos_Produtoid",
+                table: "Produtos",
+                column: "Produtoid",
+                principalTable: "Produtos",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
